20dae7e202fcf782829c58b933cafd33dfdb881b
Jake Carns <jacob.carns@gmail.com>
2018-03-08 17:44:24 -0700

    added help command, added server and client event hooks

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 const Eureca = require('eureca.io');
-let client = new Eureca.Client({ uri: 'http://localhost:8000/' });
+let client = new Eureca.Client({ uri: process.argv[2] });
 
 client.exports.user = function (user) {
     console.log(user);
@@ -19,14 +19,54 @@ client.ready(function (serverProxy) {
   rl.on('line', function(line){
 
     let args = line.split(/\s+/);
+    
+    let desc = {
+      uuid: "creates a uuid"
+    };
+
     switch(args[0]) {
       case 'uuid':
-        serverProxy.user();
+        serverProxy.uuid();
+        break;
+      case 'help':
+        console.log('list of commands');
+        console.log('-- help');
+        console.log('---- prints out this prompt');
+        for(k in serverProxy)
+          console.log('--',k);
+          console.log('----',desc[k]);
         break;
       default:
         console.log('that is not a command!');
     }
 
   });
+
+});
+
+client.onConnect(function (connection) {
+    console.log('Incomming connection from server', connection.socket.url.host);
+});
+
+// all message middleware, good for debugging
+/*client.onMessage(function (data) {
+    console.log('Received data', data);
+});*/
+
+client.onError(function (e) {
+    console.log('error', e);
+});
+
+client.onConnectionLost(function () {
+    console.log('connection lost ... will try to reconnect');
+});
+
+client.onConnectionRetry(function (socket) {
+    console.log('retrying ...');
+
+});
+
+client.onDisconnect(function (socket) {
+    console.log('Client disconnected ', connection.id);
 
 });
--- server.js
+++ server.js
@@ -9,9 +9,37 @@ const eurecaServer  = new Eureca.Server({allow:['user']});
 eurecaServer.attach(server);
  
 //functions under "exports" namespace will be exposed to client side
-eurecaServer.exports.user = function () {
+eurecaServer.exports.uuid = function () {
+  
   var client = this.clientProxy; 
+  var connection = this.connection;
+  
+  console.log(`[${connection.id}]`, 'requested uuid, generating...');
+
   client.user({uuid: uuidv4()});
-}
+
+};
+
+eurecaServer.onConnect(function (connection) {
+
+  var client = connection.clientProxy;
+
+  console.log(`[${connection.id}]`, 'client connected');
+
+});
+
+//all messages middleware, good for debugging
+/*eurecaServer.onMessage(function (msg) {
+    console.log('RECV', msg);
+});*/
+
+
+eurecaServer.onDisconnect(function (connection) {
+  console.log(`[${connection.id}]`, 'client disconnected');
+});
+
+eurecaServer.onError(function (e) {
+    console.log('an error occured', e);
+});
  
 server.listen(8000);
