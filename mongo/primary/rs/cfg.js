0f22d646ffe38af92487fac993abe7958c474e3d
Luke Bosse <lukebosse@u.boisestate.edu>
2018-04-24 13:10:22 -0600

    added tags to support write concern for all members

--- mongo/primary/rs/cfg.js
+++ mongo/primary/rs/cfg.js
@@ -1,6 +1,8 @@
 var cfg = rs.conf();
 
+cfg.members[0].tags = {'rs0': 'rs0'};
 for(var i = 1; i < cfg.members.length; i++) {
+  cfg.members[i].tags = {'rs0': 'rs0'};
   cfg.members[i].priority = 1;
   cfg.members[i].votes = 1;
 }
