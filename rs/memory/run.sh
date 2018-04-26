8a82555e9c7bb9963434ee95632973dfc632e889
Jake Carns <jacob.carns@gmail.com>
2018-04-26 12:08:42 -0600

    removed old rs directory

--- rs/memory/run.sh
+++
@@ -1,7 +0,0 @@
-./build.sh
-for ((number=0;number<$1;number++))
-  {
-    docker volume create memory$number
-    docker run -d -v memory$number:/data/db -v $2:/tmp/p3 -l p3 -t p3/memory
-  }
-exit 0
